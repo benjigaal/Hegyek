62735e06baf0eb82154b6ce76a6ce5defcb64778
benjigaal10 <benjigaal10@gmail.com>
2025-05-20 20:13:51 +0200

    first commit

---
+++ hegyekWPF/Hegycsucs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI
+{
+    public class Hegycsucs
+    {
+        public string Nev { get; private set; }
+
+        public string Hegyseg { get; private set; }
+
+        public int Magassag { get; private set; }
+
+
+        public Hegycsucs(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            Nev = darabok[0];
+            Hegyseg = darabok[1];
+            Magassag = Convert.ToInt32(darabok[2]);
+        }
+
+        //9-10.feladat
+        public bool Keres(string szo) 
+        {
+            if (Nev.Contains(szo) || Hegyseg.Contains(szo))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev} Hegység: {Hegyseg} Magasság: {Magassag}";
+        }
+    }
+}
---
+++ hegyekWPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using hegyekCLI;
+
+namespace hegyekWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Hegycsucs> hegycsucsok = new List<Hegycsucs>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            StreamReader sr = new StreamReader("hegyek.csv"); // fájl beolvasása
+            sr.ReadLine(); // fejléc átugrása
+            while (!sr.EndOfStream)
+            {
+                hegycsucsok.Add(new Hegycsucs(sr.ReadLine()));
+            }
+            sr.Close();
+            DataGrid1.ItemsSource = hegycsucsok;
+            DataGrid1.Items.Refresh();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            int magassag = Convert.ToInt32(TextBox3.Text);
+            if (magassag > 0 && magassag < 2000)
+            {
+                hegycsucsok.Add(new Hegycsucs($"{TextBox1.Text};{TextBox2.Text};{TextBox3.Text}"));
+                DataGrid1.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Nem megfelelő értékek!");
+            }
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("hegycsucsok2.csv");
+                sw.WriteLine("Hegycsúcs neve; Hegység; Magasság;");
+                for (int i = 0; i < hegycsucsok.Count; i++)
+                {
+                    sw.WriteLine(hegycsucsok[i].ToString());
+                }
+                sw.Close();
+                MessageBox.Show("A mentés sikeresen megtörtént!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
