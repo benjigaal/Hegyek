4f188eced73303f71f9dbc35373c9cbb38fe1c17
benjigaal10 <benjigaal10@gmail.com>
2025-05-20 21:27:39 +0200

    first commit

---
+++ hegyekCLI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI
+{
+    internal class Program
+    {
+        public static List<Hegycsucs> hegycsucsok = new List<Hegycsucs>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("hegyek.csv"); // fájl beolvasása
+            sr.ReadLine(); // fejléc átugrása
+            while (!sr.EndOfStream)
+            {
+                hegycsucsok.Add(new Hegycsucs(sr.ReadLine()));
+            }
+            sr.Close();
+            //8.feladat
+            foreach (var item in hegycsucsok)
+            {
+                if (item.Magassag > 950)
+                {
+                    Console.WriteLine($"{item.Nev} {item.Hegyseg} {item.Magassag}");
+                }
+            }
+
+            //11.feladat
+            Console.WriteLine("Kérem a keresett szót: ");
+            string keresettSzo = Console.ReadLine();
+            foreach (var item in hegycsucsok)
+            {
+                if (item.Keres(keresettSzo))
+                {
+                    Console.WriteLine($"{item.Nev}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hegyekCLITests/HegycsucsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hegyekCLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI.Tests
+{
+    [TestClass()]
+    public class HegycsucsTests
+    {
+        [TestMethod()]
+        [DataRow("Bükk","Fodor-hegy;Bükk-vidék;0", true)]
+        [DataRow("kő","Füstös-kő-bérc;Bükk-vidék;0", true)]
+        [DataRow("Bükk","Írott-kő;Kőszegi-hegység;0", false)]
+        public void KeresTest(string szo, string line, bool expected)
+        {
+            Hegycsucs hegycsucs = new Hegycsucs(line);
+            Assert.AreEqual(expected, hegycsucs.Keres(szo));
+        }
+    }
+}
